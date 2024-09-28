d6769b4db781d3e305ae39c6f504809a59c20e8e
c0ldn1ght <81449663+NightwindDev@users.noreply.github.com>
2024-09-28 13:49:50 -0400

    fix: make script faster by storing lib cache in memory instead of file

--- layout/Library/Application Support/rootless-patcher/repack-rootless.sh
+++ layout/Library/Application Support/rootless-patcher/repack-rootless.sh
@@ -13,8 +13,8 @@ ldid -S "${FILE}"
 LOAD_COMMANDS="$(otool -L "${FILE}")"
 INSTALL_NAME="$(otool -D "${FILE}" | grep -v -e ":$" -e "^Archive :" | head -n1)"
 
-echo "${LOAD_COMMANDS}" | tail -n +2 | grep /usr/lib/'[^/]'\*.dylib | cut -d' ' -f1 | tr -d "[:blank:]" > "${WORKING_DIR}"/._lib_cache
-echo "${LOAD_COMMANDS}" | tail -n +2 | grep /usr/local/lib/'[^/]'\*.dylib | cut -d' ' -f1 | tr -d "[:blank:]" >> "${WORKING_DIR}"/._lib_cache
+LIB_CACHE="$(echo "${LOAD_COMMANDS}" | tail -n +2 | grep /usr/lib/'[^/]'\*.dylib | cut -d' ' -f1 | tr -d "[:blank:]")"
+LIB_CACHE="${LIB_CACHE}$(echo "${LOAD_COMMANDS}" | tail -n +2 | grep /usr/local/lib/'[^/]'\*.dylib | cut -d' ' -f1 | tr -d "[:blank:]")"
 
 if [ -n "${INSTALL_NAME}" ]; then
     install_name_tool -id @rpath/"$(basename "${INSTALL_NAME}")" "${FILE}" > /dev/null 2>&1
@@ -32,26 +32,24 @@ if echo "${LOAD_COMMANDS}" | grep -q Cephei.framework; then
     install_name_tool -change /usr/lib/Cephei.framework/Cephei @rpath/Cephei.framework/Cephei "${FILE}" > /dev/null 2>&1
 fi
 
-echo "${LOAD_COMMANDS}" | grep "/Library/PreferenceBundles" | grep -E '/[^/]*/[^/]*\.bundle' | cut -d' ' -f1  | cut -d':' -f1 | while read -r BUNDLE_PATH; do
+echo "${LOAD_COMMANDS}" | grep -F "/Library/PreferenceBundles" | grep -E '/[^/]*/[^/]*\.bundle' | cut -d' ' -f1 | cut -d':' -f1 | while read -r BUNDLE_PATH; do
     BUNDLE_NAME=$(basename "${BUNDLE_PATH}")
     install_name_tool -change "${BUNDLE_PATH}" "@rpath/${BUNDLE_NAME}.bundle/${BUNDLE_NAME}" "${FILE}" > /dev/null 2>&1
 done
 
-echo "${LOAD_COMMANDS}" | grep "/Library/MobileSubstrate/DynamicLibraries" | grep -E '/[^/]*' | cut -d' ' -f1  | cut -d':' -f1 | while read -r DYLIB_PATH; do
+echo "${LOAD_COMMANDS}" | grep -F "/Library/MobileSubstrate/DynamicLibraries" | grep -E '/[^/]*' | cut -d' ' -f1 | cut -d':' -f1 | while read -r DYLIB_PATH; do
     DYLIB_NAME=$(basename "${DYLIB_PATH}")
     install_name_tool -change "${DYLIB_PATH}" "@rpath/${DYLIB_NAME}" "${FILE}" > /dev/null 2>&1
 done
 
-echo "${LOAD_COMMANDS}" | grep "/Library/Frameworks" | grep -E '/[^/]*/[^/]*\.framework' | grep -v "/System/Library/Frameworks" | cut -d' ' -f1  | cut -d':' -f1 | while read -r FRAMEWORK_PATH; do
+echo "${LOAD_COMMANDS}" | grep -F "/Library/Frameworks" | grep -E '/[^/]*/[^/]*\.framework' | grep -v "/System/Library/Frameworks" | cut -d' ' -f1 | cut -d':' -f1 | while read -r FRAMEWORK_PATH; do
     FRAMEWORK_NAME=$(basename "${FRAMEWORK_PATH}")
     install_name_tool -change "${FRAMEWORK_PATH}" "@rpath/${FRAMEWORK_NAME}.framework/${FRAMEWORK_NAME}" "${FILE}" > /dev/null 2>&1
 done
 
-if [ -f "${WORKING_DIR}"/._lib_cache ]; then
-    cat "${WORKING_DIR}"/._lib_cache | while read LINE; do
-        install_name_tool -change "${LINE}" @rpath/"$(basename "${LINE}")" "${FILE}" > /dev/null 2>&1
-    done
-fi
+echo "${LIB_CACHE}" | while read LINE; do
+    install_name_tool -change "${LINE}" @rpath/"$(basename "${LINE}")" "${FILE}" > /dev/null 2>&1
+done
 
 install_name_tool -add_rpath "/usr/local/lib" "${FILE}" > /dev/null 2>&1
 install_name_tool -add_rpath "/usr/lib" "${FILE}" > /dev/null 2>&1
@@ -60,7 +58,5 @@ install_name_tool -add_rpath "/var/jb/usr/local/lib" "${FILE}" > /dev/null 2>&1
 install_name_tool -add_rpath "/var/jb/Library/Frameworks" "${FILE}" > /dev/null 2>&1
 install_name_tool -add_rpath "/var/jb/Library/PreferenceBundles" "${FILE}" > /dev/null 2>&1
 install_name_tool -add_rpath "/var/jb/Library/MobileSubstrate/DynamicLibraries" "${FILE}" > /dev/null 2>&1
-
-rm -rf "${WORKING_DIR}/._lib_cache"
 
 echo "[+] Success!"
